1c9502664a2748d145ca18c6fdbc978b4b8077d3
imranrehmani02 <mo.imranrehmani02@gmail.com>
2020-05-28 18:55:02 +0530

    add whole project mptourism angular (first)

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {HomeComponent} from './home/home.component';
+import {ServicesComponent} from './services/services.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component'
+
+
+
+const routes: Routes = [
+{
+	path:"",
+	component:HomeComponent
+},
+
+{
+	path:"services",
+	component:ServicesComponent
+},
+
+{
+	path:"about",
+	component:AboutComponent
+},
+
+{
+	path:"contact",
+	component:ContactComponent
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
